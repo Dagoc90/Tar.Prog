3e19abfc9ff73be5a3b9754525386e8bed286e27
Dagoc90 <alfredo.ortizc@uanl.edu.mx>
2025-01-30 14:08:36 -0600

    Create Actividad3.py

---
+++ Semana 1/Actividad3.py
@@ -0,0 +1,50 @@
+print ("Hola Mundo!")
+
+#1
+print ("¿Cual es la funcion de la memoria secundaria en una computadora?")
+print ("Almacenar informacion durante mucho tiempo, incluso despues de ciclos de apagado y encendido.")
+
+
+#2
+print ("¿Que es un programa?")
+print ("Un programa es una declarcion de sentencias diseñadas para hacer algo.")
+
+
+#3
+print ("¿Cual es la diferencia entre un compilador y un interprete?")
+print ("Un interprete lee el codigo fuente de los programas como lo escribio el programador, lo analiza e interpreta las intrucciones. Un compilador ocupa el programa completo, luego lo convierte a codigo maquina, para posteriormente ejecutarse.")
+
+
+#4
+print ("¿Cua ́l de los siguientes contiene codigo maquina?")
+print ("El codigo fuente de Python.")
+
+
+#5
+print ("¿Que esta mal en el siguiente codigo?:")
+print ("Esta escrito print con m, y esta entre apostrofes en vez de comillas")
+
+
+#6
+print ("¿En que lugar del computador queda almacenada una variable, como en este caso x despues de ejecutar la siguiente lınea de Python?:")
+print ("Memoria Principal.")
+
+
+#7
+print ("¿Que mostrara en pantalla el siguiente programa?")
+print ("44")
+
+
+#8
+print ("Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:")
+print ("Unidad central de procesamiento:   Nuestro cerebro (?)")
+print ("Memoria principal:    Las acciones que hacemos despiertos (?)")
+print ("Memoria secundaria:   Las acciones como respirar y parpadear (?)")
+print ("Dispositivos de entrada:   Nuestros ojos, oidos y piel")
+print ("Dispositivos de salida:   Boca, manos, pies")
+ # Esta pregunta no se si la entendi bien. Supongo queda a su criterio.
+
+
+#9
+print ("¿Como puedes corregir un “Error de sintaxis”?")
+print ("Buscando el error cometido y corrigiendolo. Generalmente el programa te avisa cuando se comete el error.")
